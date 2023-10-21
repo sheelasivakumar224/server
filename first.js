7e416c4ab738794827ffa806cac15d18c4d36bb9
Sheela Sivakumar <125798345+sheelasivakumar224@users.noreply.github.com>
2023-10-21 16:00:30 +0530

    Delete first.js

--- first.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const myVariable = process.env.SERVER;
-
-if (myVariable) {
-  console.log(`The value of MY_VARIABLE is: ${myVariable}`);
-} else {
-  console.log('MY_VARIABLE is not set.');
-}
