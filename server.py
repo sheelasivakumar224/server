8e3335a2c8c62922371600ac312e008c44d586f4
Sheela Sivakumar <125798345+sheelasivakumar224@users.noreply.github.com>
2023-10-21 18:54:55 +0530

    Update server.py

--- server.py
+++ server.py
@@ -15,9 +15,34 @@ app = Flask(__name__)
 app.secret_key = "xyzabcdefghijkl"
 
 
+TYPE=os.getenv("TYPE")
+PROJECT_ID=os.getenv("PROJECT_ID")
+PRIVATE_KEY_ID=os.getenv("PRIVATE_KEY_ID")
+PRIVATE_KEY=os.getenv("PRIVATE_KEY")
+CLIENT_EMAIL=os.getenv("CLIENT_EMAIL")
+CLIENT_ID_FIREBASE=os.getenv("")
+AUTH_URI=os.getenv("AUTH_URI")
+TOKEN_URI=os.getenv("TOKEN_URI")
+AUTH_PROVIDER_x509_CERT_URL=os.getenv("AUTH_PROVIDER_x509_CERT_URL")
+CLIENT_x509_CERT_URL=os.getenv("CLIENT_x509_CERT_URL")
+UNIVERSE_DOMAIN=os.getenv("UNIVERSE_DOMAIN")
+
+
 '''Firestore'''
 
-cred = credentials.Certificate("login.json")
+cred = credentials.Certificate({
+    "type": TYPE,
+    "project_id": PROJECT_ID,
+    "private_key_id": PRIVATE_KEY_ID,
+    "private_key":PRIVATE_KEY ,
+    "client_email":CLIENT_EMAIL,
+    "client_id":CLIENT_ID_FIREBASE,
+    "auth_uri":AUTH_URI,
+    "token_uri": TOKEN_URI,
+    "auth_provider_x509_cert_url":AUTH_PROVIDER_x509_CERT_URL ,
+    "client_x509_cert_url": CLIENT_x509_CERT_URL,
+    "universe_domain":UNIVERSE_DOMAIN 
+})
 firebase_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
