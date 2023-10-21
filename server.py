d985f742707f1e8019982b9e2b485b753fd9ff7e
Sheela Sivakumar <125798345+sheelasivakumar224@users.noreply.github.com>
2023-10-21 16:14:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -17,19 +17,7 @@ app.secret_key = "xyzabcdefghijkl"
 
 '''Firestore'''
 
-cred = credentials.Certificate({
-    "type" : os.getenv("TYPE"),
-    "project_id" : os.getenv("PROJECT_ID"),
-    "private_key_id" : os.getenv("PRIVATE_KEY_ID"),
-    "private_key" : os.getenv("PRIVATE_KEY"),
-    "client_email" : os.getenv("CLIENT_EMAIL"),
-    "client_id" : os.getenv("CLIENT_ID_FIREBASE"),
-    "auth_uri" : os.getenv("AUTH_URI"),
-    "token_uri" :os.getenv("TOKEN_URI"),
-    "auth_provider_x509_cert_url" : os.getenv("AUTH_PROVIDER_x509_CERT_URL"),
-    "client_x509_cert_url" : os.getenv("CLIENT_x509_CERT_URL"),
-    "universe_domain" : os.getenv("UNIVERSE_DOMAIN")
-})
+cred = credentials.Certificate("login.json")
 firebase_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
